61e7c4dc58c0918e2dcd9cd884619949d9b6fddd
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-10-20 17:20:58 -0300

    Don't rely on Array.prototype.reduce

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ export function pipe<a,b,c,d,e,f,g,h>       (x: a, f0: fn<a,b>, f1: fn<b,c>, f2:
 export function pipe<a,b,c,d,e,f,g,h,i>     (x: a, f0: fn<a,b>, f1: fn<b,c>, f2: fn<c,d>, f3: fn<d,e>, f4: fn<e,f>, f5: fn<f,g>, f6: fn<g,h>, f7: fn<h,i>)                           : i;
 export function pipe<a,b,c,d,e,f,g,h,i,j>   (x: a, f0: fn<a,b>, f1: fn<b,c>, f2: fn<c,d>, f3: fn<d,e>, f4: fn<e,f>, f5: fn<f,g>, f6: fn<g,h>, f7: fn<h,i>, f8: fn<i,j>)              : j;
 export function pipe<a,b,c,d,e,f,g,h,i,j,k> (x: a, f0: fn<a,b>, f1: fn<b,c>, f2: fn<c,d>, f3: fn<d,e>, f4: fn<e,f>, f5: fn<f,g>, f6: fn<g,h>, f7: fn<h,i>, f8: fn<i,j>, f9: fn<j,k>) : k;
-export function pipe(x:any, ...fs:any[]) {
-	return fs.reduce((y,f) => f(y), x);
+export function pipe(x: any) {
+	let y = x;
+	for (let i = 1, len = arguments.length; i < len; i += 1) {
+		y = arguments[i](y);
+	}
+	return y;
 }
